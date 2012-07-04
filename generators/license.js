c441b2852e5d9ac63af22231769f53f39e385844
Louis Coquio <louis.coquio@gmail.com>
2012-07-04 18:43:54 +0200

    license.js

---
+++ generators/license.js
@@ -0,0 +1,6 @@
+// ==========================================================================
+// Project:   Ember Validations
+// Copyright: ©2012 Louis Coquio and contributors
+//            Portions ©2006-2011 Strobe Inc.
+// License:   Licensed under MIT license (see license.js)
+// ==========================================================================
