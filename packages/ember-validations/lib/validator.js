9cd2109c980521b99a0908dd27c1407171d2effd
Louis Coquio <louis.coquio@gmail.com>
2012-07-12 23:15:35 +0200

    Add Validator#checkValidity call on #init if set

--- packages/ember-validations/lib/validator.js
+++ packages/ember-validations/lib/validator.js
@@ -1,5 +1,12 @@
 Ember.Validator = Ember.Object.extend({
 
+    init: function() {
+        this._super();
+        if (this.checkValidity) {
+            this.checkValidity();
+        }
+    },
+
     validate: function() {
         throw new Error("Ember.Validator subclasses should implement validate() method.");
     }
--- packages/ember-validations/tests/validator_test.js
+++ packages/ember-validations/tests/validator_test.js
@@ -10,3 +10,11 @@ module("Ember.Validator", {
 test("Direct instances should raise an error on validate", function() {
     raises(function() { validator.validate(); },  /Ember.Validator subclasses should implement validate\(\) method./);
 });
+
+test("checkValidity should be called on #init if set", function() {
+    validator = Ember.Validator.create({
+        checkValidity: function() {
+            ok(true, "checkValidity should be called");
+        }
+    });
+})
