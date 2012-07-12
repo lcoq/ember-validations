2af813b14ac6531b79f6746218ce3309a0a5b941
Louis Coquio <louis.coquio@gmail.com>
2012-07-12 19:19:51 +0200

    Remove Validator.options default value

--- packages/ember-validations/lib/validator.js
+++ packages/ember-validations/lib/validator.js
@@ -1,10 +1,4 @@
 Ember.Validator = Ember.Object.extend({
-    init: function() {
-        this._super();
-        if (!this.get('options')) {
-            this.set('options', {});
-        }
-    },
 
     validate: function() {
         throw new Error("Ember.Validator subclasses should implement validate() method.");
--- packages/ember-validations/tests/validator_test.js
+++ packages/ember-validations/tests/validator_test.js
@@ -10,7 +10,3 @@ module("Ember.Validator", {
 test("Direct instances should raise an error on validate", function() {
     raises(function() { validator.validate(); },  /Ember.Validator subclasses should implement validate\(\) method./);
 });
-
-test("Options should be an empty hash by default", function() {
-    deepEqual(validator.get('options'), {});
-});
