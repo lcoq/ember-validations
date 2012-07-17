6bcb292775d760bba94ea8195c7d7df930ef8dfe
Louis Coquio <louis.coquio@gmail.com>
2012-07-17 22:37:29 +0200

    Add Ember.Validations doc

--- packages/ember-validations/lib/validations.js
+++ packages/ember-validations/lib/validations.js
@@ -1,3 +1,61 @@
+/**
+   @class
+
+   This mixin is used to handle validations on ember objects.
+   If you are implementing an object and want to support validations, just include
+   this mixin in your object and set the `validations` property.
+
+   Here is an example of an object using validations :
+
+     Ember.Object.create(Ember.Validations, {
+       validations: {
+         name: {presence: true}
+       }
+     })
+
+   When calling `validate` method on the object above, the method `Ember.Validators.PresenceValidator.validate`
+   is called, and add error messages if the `name` of the object is not present.
+
+   Its possible to pass options to the validator, as shown in this example :
+
+     validations: {
+     name: {
+       length: {
+         moreThan: 3,
+         lessThan: 10
+       }
+     }
+
+
+   You could also set custom validations and pass options, like in this example :
+
+     validations: {
+       amount: {
+         validator: MyApp.CustomValidator,
+         options: {
+           isNumber: true,
+           otherOption: 12
+         }
+       }
+     }
+
+   Or even directly set the validation function :
+
+     validations: {
+       amount: {
+         validator: function(obj, attr, value) {
+           var moreThan = this.getPath('options.moreThan');
+           if (value <= moreThan) {
+             obj.get('errors').add(attr, "should not be falsy");
+           }
+         },
+         options: {
+           moreThan: 5
+         }
+       }
+     }
+
+ */
 Ember.Validations = Ember.Mixin.create({
     init: function() {
         this._super();
