05a4831ca4396742d05ed6d4edea56b814e74bdf
Louis Coquio <louis.coquio@gmail.com>
2012-07-28 16:48:23 +0200

    PresentValidatorTest performance improvment

--- packages/ember-validations/tests/validators/presence_test.js
+++ packages/ember-validations/tests/validators/presence_test.js
@@ -1,3 +1,5 @@
+var get = Ember.get, getPath = Ember.getPath, set = Ember.set;
+
 var model, validator, moduleOpts = {
   setup: function() {
     model = Ember.Object.create({errors: Ember.ValidationErrors.create()});
@@ -13,21 +15,21 @@ module("Ember.Validators.PresenceValidator", moduleOpts);
 test("should add error when the attribute is not present", function() {
   var invalidValues = [undefined, null, '', ' ', '  '];
   invalidValues.forEach(function(val, index) {
-    model.set('errors', Ember.ValidationErrors.create());
+    set(model, 'errors', Ember.ValidationErrors.create());
     validator.validate(model, 'name', val);
 
-    var errors = model.getPath('errors.name');
+    var errors = getPath(model, 'errors.name');
 
-    var errorKeys = errors.get('keys');
+    var errorKeys = get(errors, 'keys');
     equal(errorKeys.length, 1, "has one error");
     equal(errorKeys[0], "blank", "has right key");
 
-    var errorMessage = errors.get('messages');
+    var errorMessage = get(errors, 'messages');
     equal(errorMessage[0], "can't be blank", "has right message");
   });
 });
 
 test("should not add error when the attribute is present", function() {
   validator.validate(model, 'name', "my name");
-  equal(model.getPath('errors.name.keys'), undefined, "should not set 'name' error");
+  equal(getPath(model, 'errors.name.keys'), undefined, "should not set 'name' error");
 });
