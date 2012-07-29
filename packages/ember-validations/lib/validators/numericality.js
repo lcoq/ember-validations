95a9a05d8970e8621bc83b748d49347d5bae685a
Louis Coquio <louis.coquio@gmail.com>
2012-07-29 23:25:50 +0200

    Add NumericalityValidator greaterThan/lowerThan (orEqualTo) options

--- packages/ember-validations/lib/validators/numericality.js
+++ packages/ember-validations/lib/validators/numericality.js
@@ -1,21 +1,39 @@
 var get = Ember.get;
 
-Ember.ValidationError.addMessage('notANumber', "is not a number");
-Ember.ValidationError.addMessage('notAnInteger', "is not an integer");
+Ember.ValidationError.addMessages({
+  'notNumber': "is not a number",
+  'notInteger': "is not an integer",
+  'notGreaterThan': "is not greater than @{value}",
+  'notGreaterThanOrEqualTo': "is not greater than or equal to @{value}",
+  'notLessThan': "is not less than @{value}",
+  'notLessThanOrEqualTo': "is not less than or equal to @{value}"
+});
 
 Ember.Validators.NumericalityValidator = Ember.Validator.extend({
 
   validate: function(obj, attr, value) {
-    var parsedFloat = parseFloat(value),
+    var parsedValue = parseFloat(value),
         parsedInt = parseInt(value, 10),
         errors = get(obj, 'errors');
 
-    if (isNaN(value) || isNaN(parsedFloat)) {
-      errors.add(attr, 'notANumber');
+    if (isNaN(value) || isNaN(parsedValue)) {
+      errors.add(attr, 'notNumber');
     } else {
       var options = get(this, 'options');
-      if (options.onlyInteger === true && (parsedFloat !== parsedInt)) {
-        errors.add(attr, 'notAnInteger');
+      if (options.onlyInteger === true && (parsedValue !== parsedInt)) {
+        errors.add(attr, 'notInteger');
+      }
+      if (typeof options.greaterThan === 'number' && parsedValue <= options.greaterThan) {
+        errors.add(attr, 'notGreaterThan', {value: options.greaterThan});
+      }
+      if (typeof options.greaterThanOrEqualTo === 'number' && parsedValue < options.greaterThanOrEqualTo) {
+        errors.add(attr, 'notGreaterThanOrEqualTo', {value: options.greaterThanOrEqualTo});
+      }
+      if (typeof options.lessThan === 'number' && parsedValue >= options.lessThan) {
+        errors.add(attr, 'notLessThan', {value: options.lessThan});
+      }
+      if (typeof options.lessThanOrEqualTo === 'number' && parsedValue > options.lessThanOrEqualTo) {
+        errors.add(attr, 'notLessThanOrEqualTo', {value: options.lessThanOrEqualTo});
       }
     }
   }
--- packages/ember-validations/tests/validators/numericality_test.js
+++ packages/ember-validations/tests/validators/numericality_test.js
@@ -1,5 +1,18 @@
 var get = Ember.get, getPath = Ember.getPath, set = Ember.set;
 
+var hasOneError = function(model, attr, key, msg) {
+  var errorsPath = 'errors.' + attr;
+  var errors = getPath(model, errorsPath);
+  ok(errors, "has an " + errorsPath + " object");
+
+  var errorKeys = getPath(model, errorsPath + '.keys');
+  equal(errorKeys.length, 1, "has one error");
+  equal(errorKeys[0], key, "has right key");
+
+  var errorMsgs = getPath(model, errorsPath + '.messages');
+  equal(errorMsgs[0], msg, "has right message");
+};
+
 var model, validator, moduleOpts = {
   setup: function() {
     model = Ember.Object.create({errors: Ember.ValidationErrors.create()});
@@ -14,15 +27,7 @@ module("Ember.Validators.NumericalityValidator", moduleOpts);
 
 test("attribute is not a valid number", function() {
   validator.validate(model, 'amount', "amount");
-
-  // attribute is not a number
-  var errors = getPath(model, 'errors.amount');
-  ok(errors, "has an errors.amount object");
-
-  var errorKeys = getPath(model, 'errors.amount.keys');
-  equal(errorKeys.length, 1, "has one error");
-  equal(errorKeys[0], 'notANumber', "has right key");
-  equal(get(errors, 'messages')[0], "is not a number", "has right message");
+  hasOneError(model, 'amount', 'notNumber', "is not a number");
 });
 
 test("attribute is a valid number", function() {
@@ -33,12 +38,35 @@ test("attribute is a valid number", function() {
 test("onlyInteger option with float number", function() {
   set(validator, 'options', {onlyInteger: true});
   validator.validate(model, 'amount', "54.321");
+  hasOneError(model, 'amount', 'notInteger', "is not an integer");
+});
 
-  var errors = getPath(model, 'errors.amount');
-  ok(errors, "has an errors.amount object");
+test("greaterThan option", function() {
+  set(validator, 'options', {greaterThan: 12});
+  validator.validate(model, 'amount', "12");
+  hasOneError(model, 'amount', 'notGreaterThan', "is not greater than 12");
+});
 
-  var errorKeys = getPath(model, 'errors.amount.keys');
-  equal(errorKeys.length, 1, "has one error");
-  equal(errorKeys[0], 'notAnInteger', "has right key");
-  equal(get(errors, 'messages')[0], "is not an integer", "has right message");
+test("greaterThanOrEqualTo option with less", function() {
+  set(validator, 'options', {greaterThanOrEqualTo: 12});
+  validator.validate(model, 'amount', "4");
+  hasOneError(model, 'amount', 'notGreaterThanOrEqualTo', "is not greater than or equal to 12");
+});
+
+test("greaterThanOrEqualTo option with equal", function() {
+  set(validator, 'options', {greaterThanOrEqualTo: 12});
+  validator.validate(model, 'amount', "12");
+  ok(!getPath(model, 'errors.amount'), "has no amount errors");
+});
+
+test("lessThan option", function() {
+  set(validator, 'options', {lessThan: 12});
+  validator.validate(model, 'amount', "12");
+  hasOneError(model, 'amount', 'notLessThan', "is not less than 12");
+});
+
+test("lessThanOrEqualTo option with more", function() {
+  set(validator, 'options', {lessThanOrEqualTo: 12});
+  validator.validate(model, 'amount', "13");
+  hasOneError(model, 'amount', 'notLessThanOrEqualTo', "is not less than or equal to 12");
 });
