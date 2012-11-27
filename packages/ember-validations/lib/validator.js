135131248d8c334840a5b91c93d255919dbae8e2
Sean O'Hara <sohara@sohara.com>
2012-11-27 18:07:49 -0500

    Update API docs to reflect change to code (Validators must implemment _validate instead of validate).

--- packages/ember-validations/lib/validator.js
+++ packages/ember-validations/lib/validator.js
@@ -3,12 +3,12 @@
 
    This class is used by `Ember.Validations` to validate attributes of an object.
 
-   Subclasses should implement `validate` method, and add an error when the object is not valid.
+   Subclasses should implement the private `_validate` method, and add an error when the object is not valid.
    It could be done with `add` method of `Ember.ValidationErrors` class, as shown in this example:
 
 
        App.TruthyValidator = Ember.Validator.extend({
-         validate: function(obj, attr, value) {
+         _validate: function(obj, attr, value) {
            if (!value) {
              obj.get('validationErrors').add(attr, "should be truthy");
            }
